5113c210ee66bdbc16a6c83e4148ddf1ad5ac544
Hyuntak Cha <chaht01@gmail.com>
2018-05-14 23:37:53 +0900

    Fixed #4 Api configuration files

--- src/config/Api/index.js
+++ src/config/Api/index.js
@@ -1,9 +1,13 @@
 import {authorize, verify} from './authorize'
+import {gym} from './gym'
+import {problem} from './problem'
 import { storeItem, clearItem } from './storage'
 import asyncTest from './asyncTest'
 import fetchApi from './fetchApi'
 const Api = {
     authorize,
+    gym,
+    problem,
     verify,
     storeItem,
     clearItem,
